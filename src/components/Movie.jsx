71ed61ef61830150839897f6caa82088620947e6
ptahe <coticotico20@gmail.com>
2024-03-15 17:33:36 +0300

    first commit

---
+++ src/components/Movie.jsx
@@ -0,0 +1,33 @@
+function Movie(props) {
+    const {
+        Title: title,
+        Year: year,
+        imbdbID: id,
+        Type: type,
+        Poster: poster
+    } = props;
+
+
+    return <div id={id} className="movie card">
+        <div className="card-image waves-effect waves-block waves-light">
+            {
+                poster === 'N/A' ? (
+                    <img src={`https://via.placeholder.com/300x400?text=${title}`} 
+                    alt=""
+                    className="activator" />
+                ) :
+                <img src={poster} alt="" className="activator" />
+            }
+        </div>
+        <div className="card-content">
+            <span className="card-title activator grey-text text-darken-4">
+                {title}
+            </span>
+            <p>
+                {year} <span className="right">{type}</span>
+            </p>
+        </div>
+    </div>;
+}
+
+export { Movie };
---
+++ src/layout/header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return <nav className=" cyan darken-4">
+    <div className="container nav-wrapper">
+      <a href="!#" className="brand-logo right">Movies</a>
+      <ul id="nav-mobile" className="left">
+        <li><a href="!#">Films</a></li>
+      </ul>
+    </div>
+    </nav>
+}
+export {Header};
