3c54f8a3857308c959529870ad25bff7bbf1fb9c
ptahe <120496021+ptahe@users.noreply.github.com>
2024-03-15 23:08:14 +0300

    Update main.jsx

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -10,7 +10,7 @@ class Main extends React.Component{
         loading: true,
     };
     componentDidMount(){
-        fetch('http://www.omdbapi.com/?apikey=ba9ed4b0&s=land')
+        fetch('https://www.omdbapi.com/?apikey=ba9ed4b0&s=land')
             .then((response) => response.json())
             .then((data) => this.setState({ movies: data.Search, loading: false}));
     };
@@ -18,7 +18,7 @@ class Main extends React.Component{
     searchMovies = (str, type = "all") => {
         this.setState({ loading: true });
         fetch(
-            `http://www.omdbapi.com/?apikey=ba9ed4b0&s=${str}${
+            `https://www.omdbapi.com/?apikey=ba9ed4b0&s=${str}${
                 type !== "all" ? `&type=${type}` : ""
             }`
         )
